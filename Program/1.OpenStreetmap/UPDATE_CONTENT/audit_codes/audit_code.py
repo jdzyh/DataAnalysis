54b8003fb4f7e5d3a08f990c3fecfe4e674816ca
jdzyh <dtzyhy@163.com>
2017-12-20 22:32:25 +0800

    2017.12.20. P1 upload again.

--- Program/1.OpenStreetmap/UPDATE_CONTENT/audit_codes/audit_code.py
+++ Program/1.OpenStreetmap/UPDATE_CONTENT/audit_codes/audit_code.py
@@ -1,4 +1,102 @@
 #############################################
+# 数据清洗函数
+#############################################
+import re
+#用于替换value字段内的奇怪字符
+VALUE_REPLACE_CHARS = '[📮]'
+def reject_bad_chars_of_value(src_string, badchars=VALUE_REPLACE_CHARS):
+    result = re.sub(badchars, "", src_string)
+    return result
+#print(reject_bad_chars("邮筒📮"))
+
+#用于去除错误邮编
+REGULAR_POSTCODE =re.compile(r'^[0-9]{6}$')
+def is_postcode(src_postcode, regular_postcode=REGULAR_POSTCODE):
+    postcode = re.search(regular_postcode, src_postcode)
+    if postcode:
+        return True
+    else:
+        return False
+#print(is_postcode('12345 '))
+
+#用于规范电话的格式
+#如果校验通过，返回正确格式的电话，否则返回空字符串
+REGULER_PHONE = re.compile(r'^(86)?(021|21)?([0-9]{8}|[0-9]{11}|400[0-9]{7}|[0-9]{5})$')
+def audit_phone(src_phone, regular_phone=REGULER_PHONE):
+    new_phone = re.sub(r'[+ \-()]', '', src_phone)
+    phone = re.search(regular_phone, new_phone)
+    if phone:
+        phone_num = phone.group(3)
+        phone_type = len(phone_num)
+        #固话
+        if phone_type==8:
+            return '+86-021-' + phone_num
+        #手机
+        elif phone_type==11:
+            return '+86-' + phone_num
+        #400电话
+        elif phone_type==10:
+            return '+86-' + phone_num
+        #全国通用电话
+        elif phone_type==5:
+            return '+86-' + phone_num
+        else:
+            return ''
+    else:
+        return ''
+
+#专用于处理value值函数
+def value_process(current_tag):
+    current_tag['value'] = reject_bad_chars_of_value(current_tag['value'])
+    
+    #规范电话格式
+    if current_tag['key']=='phone':
+        tmp_phone = audit_phone(current_tag['value'])
+        if tmp_phone!='':
+            current_tag['value'] = tmp_phone
+            return current_tag
+        else:
+            print('错误的电话格式：' + str(current_tag))
+            return None
+    #去除错误的邮编
+    elif current_tag['key']=='postcode':
+        if is_postcode(current_tag['value']):
+            return current_tag
+        else:
+            print('错误的邮编格式' + str(current_tag))
+            return None
+    #其他情况默认保留
+    else:
+        return current_tag
+
+#专用于处理tag节点的函数
+def tag_process(element, problem_chars, tags):
+    for tag in element.iter("tag"):
+        key_string = tag.get('k')
+        if re.search(problem_chars, key_string):
+            continue
+    
+        current_tag = {}
+        current_tag['id'] = element.get('id')
+        #去除value中的坏字符
+        current_tag['value'] = tag.get('v')
+
+        #根据k的值确定key与type
+        if key_string.find(':')<0:
+            current_tag['key'] = key_string
+            current_tag['type'] = "regular"
+        else:
+            m = re.search(NODE_TAG_COLON, key_string)
+            current_tag['key'] = m.group(2)
+            current_tag['type'] = m.group(1)
+        
+        #处理value的值
+        new_tag = value_process(current_tag)
+        if new_tag != None:
+            tags.append(new_tag)
+    
+    return tags
+#############################################
 # 数据清洗并生成CSV文件的代码
 #############################################
 import csv
@@ -47,62 +145,26 @@ REL_MEMBER_FIELDS = ['id', 'ref', 'type', 'role', 'position']
 def shape_element(element, node_attr_fields=NODE_FIELDS, way_attr_fields=WAY_FIELDS, rel_attr_fields=REL_FIELDS,
                   problem_chars=PROBLEMCHARS, default_tag_type='regular'):
     node_attribs = {}
-    
     way_attribs = {}
     way_nodes = []
-    
     rel_attribs = {}
     rel_members = []
-    
     tags = []  # Handle secondary tags the same way for both node and way elements
 
     # 检查 node 节点
     if element.tag == 'node':
         for node_attr_field in node_attr_fields:
             node_attribs[node_attr_field] = element.get(node_attr_field)
-        for tag in element.iter("tag"):
-            key_string = tag.get('k')
-            if re.search(PROBLEMCHARS, key_string):
-                continue
-            if key_string.find(':')<0:
-                current_tag = {}
-                current_tag['id'] = element.get('id')
-                current_tag['value'] = tag.get('v')
-                current_tag['key'] = key_string
-                current_tag['type'] = "regular"
-                tags.append(current_tag)
-            else:
-                current_tag = {}
-                m = re.search(NODE_TAG_COLON, key_string)
-                current_tag['id'] = element.get('id')
-                current_tag['value'] = tag.get('v')
-                current_tag['key'] = m.group(2)
-                current_tag['type'] = m.group(1)
-                tags.append(current_tag)
+        #处理该element节点的tag子节点
+        tags = tag_process(element, problem_chars, tags)
         return {'node': node_attribs, 'node_tags': tags}
+    
     #检查 way 节点    
     elif element.tag == 'way':
         for way_attr_field in way_attr_fields:
             way_attribs[way_attr_field] = element.get(way_attr_field)
-        for tag in element.iter("tag"):
-            key_string = tag.get('k')
-            if re.search(PROBLEMCHARS, key_string):
-                continue
-            if key_string.find(':')<0:
-                current_tag = {}
-                current_tag['id'] = element.get('id')
-                current_tag['value'] = tag.get('v')
-                current_tag['key'] = key_string
-                current_tag['type'] = "regular"
-                tags.append(current_tag)
-            else:
-                current_tag = {}
-                m = re.search(NODE_TAG_COLON, key_string)
-                current_tag['id'] = element.get('id')
-                current_tag['value'] = tag.get('v')
-                current_tag['key'] = m.group(2)
-                current_tag['type'] = m.group(1)
-                tags.append(current_tag)
+        #处理该element节点的tag子节点
+        tags = tag_process(element, problem_chars, tags)
         cnt=0
         for node in element.iter("nd"):
             way_node = {}
@@ -111,7 +173,6 @@ def shape_element(element, node_attr_fields=NODE_FIELDS, way_attr_fields=WAY_FIE
             way_node['position'] = cnt
             cnt+=1
             way_nodes.append(way_node)
-            
         return {'way': way_attribs, 'way_nodes': way_nodes, 'way_tags': tags}
     
     #检查 relation 节点
@@ -119,26 +180,8 @@ def shape_element(element, node_attr_fields=NODE_FIELDS, way_attr_fields=WAY_FIE
         #relation节点的属性
         for rel_attr_field in rel_attr_fields:
             rel_attribs[rel_attr_field] = element.get(rel_attr_field)
-        #迭代tag子节点
-        for tag in element.iter('tag'):
-            key_string = tag.get('k')
-            if re.search(PROBLEMCHARS, key_string):
-                continue
-            if key_string.find(':')<0:
-                current_tag = {}
-                current_tag['id'] = element.get('id')
-                current_tag['value'] = tag.get('v')
-                current_tag['key'] = key_string
-                current_tag['type'] = "regular"
-                tags.append(current_tag)
-            else:
-                current_tag = {}
-                m = re.search(NODE_TAG_COLON, key_string)
-                current_tag['id'] = element.get('id')
-                current_tag['value'] = tag.get('v')
-                current_tag['key'] = m.group(2)
-                current_tag['type'] = m.group(1)
-                tags.append(current_tag)
+        #处理该element节点的tag子节点
+        tags = tag_process(element, problem_chars, tags)
         #迭代member子节点
         cnt=0
         for member in element.iter('member'):
